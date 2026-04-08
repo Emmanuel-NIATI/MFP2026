1b68695dae3035a48170b11c0ea23a477611370b
Emmanuel-NIATI <emmanuel.niati@outlook.fr>
2026-04-08 22:32:05 +0200

    V100 : Initialisation du Projet

---
+++ 04_AFF/tm1637_library_01.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+
+"""
+
+import RPi.GPIO as GPIO
+from tm1637 import TM1637
+
+display = TM1637(clk=xx, dio=xx, brightnes=7, is_show_point=False, pin_mode=GPIO.BCM)
+
+
+display.show([1, 2, 3, 4])
+display.clear()
+
+display.show([1, 2, None, None])
+
+display.show_point()
+display.close_point()
+
+display.set_brightnes(1)
+
+display.show_data((0b1111111, 0b1111111, 0b1111111, 0b1000000))
+
+display.disable()
+display.enable()
+
+      A
+     ---
+  F |   | B
+     -G-
+  E |   | C
+     ---
+      D
+
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+class tm1637_01:
+
+    COMMAND_DATA = 0x40         # data command
+    COMMAND_ADDRESS = 0xC0      # address command
+    COMMAND_CTRL = 0x80         # display control command
+    TM1637_DSP_ON = 0x08        # display on
+    TM1637_DSP_OFF = 0x00       # display off
+
+    DIGIT_TO_HEX = {
+
+        '0': 0x3f,
+        '1': 0x06,
+        '2': 0x5b,
+        '3': 0x4f,
+        '4': 0x66,
+        '5': 0x6d,
+        '6': 0x7d,
+        '7': 0x07,
+        '8': 0x7f,
+        '9': 0x6f,
+        'A': 0x77,
+        'B': 0x7f,
+        'b': 0x7C,
+        'C': 0x39,
+        'c': 0x58,
+        'D': 0x3f,
+        'd': 0x5E,
+        'E': 0x79,
+        'F': 0x71,
+        'G': 0x7d,
+        'H': 0x76,
+        'h': 0x74,
+        'I': 0x06,
+        'J': 0x1f,
+        'K': 0x76,
+        'L': 0x38,
+        'l': 0x06,
+        'n': 0x54,
+        'O': 0x3f,
+        'o': 0x5c,
+        'P': 0x73,
+        'r': 0x50,
+        'S': 0x6d,
+        'U': 0x3e,
+        'V': 0x3e,
+        'Y': 0x66,
+        'Z': 0x5b,
+        ' ': 0x00,
+        'T1': 0x07,
+        'T2': 0x31,
+        'M1': 0x33,
+        'M2': 0x27,
+        'W1': 0x3c,
+        'W2': 0x1e,
+
+}
+
+    DIGIT_TO_SEGMENT = [
+        0b0111111,  # 0
+        0b0000110,  # 1
+        0b1011011,  # 2
+        0b1001111,  # 3
+        0b1100110,  # 4
+        0b1101101,  # 5
+        0b1111101,  # 6
+        0b0000111,  # 7
+        0b1111111,  # 8
+        0b1101111,  # 9
+        0b1110111,  # A
+        0b1111100,  # b
+        0b0111001,  # C
+        0b1011110,  # d
+        0b1111001,  # E
+        0b1110001   # F
+    ]
+
+    NONE_SEGMENT = 0b0000000
+
+    DATA_CLEAR = (0x00, 0x00, 0x00, 0x00)
+
+    def __init__(self, clk, dio, brightnes=2, is_show_point=False, pin_mode=GPIO.BCM):
+
+        assert 0 <= brightnes <= 7
+
+        self.clk = clk
+        self.dio = dio
+        self.brightnes = brightnes
+        self.is_show_point = is_show_point
+        self.display_status = self.TM1637_DSP_ON
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(pin_mode)
+        GPIO.setup(self.clk, GPIO.OUT)
+        GPIO.setup(self.dio, GPIO.OUT)
+        self.current_data = self.DATA_CLEAR
+
+
+    def enable(self):
+
+        self.display_status = self.TM1637_DSP_ON
+        self.refresh()
+
+
+    def disable(self):
+
+        self.display_status = self.TM1637_DSP_OFF
+        self.refresh()
+
+
+    def set_brightnes(self, brightnes):
+
+        assert 0 <= brightnes <= 7
+        self.brightnes = brightnes
+        self.refresh()
+
+
+    def clear(self):
+
+        self.show_data(self.DATA_CLEAR)
+
+
+    def show_point(self):
+
+        self.is_show_point = True
+        self.current_data = (self.current_data[0],
+                             self.current_data[1] | 0b10000000,
+                             self.current_data[2],
+                             self.current_data[3])
+        self.refresh()
+
+
+    def show_point(self):
+
+        point_data = 0b10000000
+        self.is_show_point = True
+        self.current_data = (self.current_data[0],
+                             self.current_data[1] | point_data,
+                             self.current_data[2],
+                             self.current_data[3])
+        self.refresh()
+
+
+    def close_point(self):
+        
+        point_data = 0b01111111
+        self.is_show_point = False
+        self.current_data = (self.current_data[0],
+                             self.current_data[1] & point_data,
+                             self.current_data[2],
+                             self.current_data[3])
+        self.refresh()
+
+
+    def show_data(self, data):
+
+        self.current_data = tuple(data)
+        self.start()
+        self.write_byte(self.COMMAND_DATA)
+        self.stop()
+        self.start()
+        self.write_byte(self.COMMAND_ADDRESS)
+
+        for i in range(4):
+            self.write_byte(data[i])
+        
+        self.stop()
+        self.start()
+        self.write_byte(self.COMMAND_CTRL | self.display_status | self.brightnes)
+        self.stop()
+    
+    
+    def show(self, data):
+
+        if data[1] is None:
+            data[1] = self.NONE_SEGMENT
+    
+        if data[2] is None:
+            data[2] = self.NONE_SEGMENT
+
+        if data[3] is None:
+            data[3] = self.NONE_SEGMENT
+
+        if data[3] is None:
+            data[3] = self.NONE_SEGMENT
+
+        encoded_data = (
+            self.DIGIT_TO_HEX[data[1]],
+            self.DIGIT_TO_HEX[data[2]],
+            self.DIGIT_TO_HEX[data[3]],
+            self.DIGIT_TO_HEX[data[4]]
+        )
+
+        self.show_data(encoded_data)
+
+    def write_byte(self, b):
+
+        for i in range(0, 8):
+
+            GPIO.output(self.clk, GPIO.LOW)
+
+            if b & 0x01:
+                GPIO.output(self.dio, GPIO.HIGH)
+            else:
+                GPIO.output(self.dio, GPIO.LOW)
+
+            b >>= 1
+            GPIO.output(self.clk, GPIO.HIGH)
+
+        GPIO.output(self.clk, GPIO.LOW)
+        GPIO.output(self.dio, GPIO.HIGH)
+        GPIO.output(self.clk, GPIO.HIGH)
+        GPIO.setup(self.dio, GPIO.IN)
+
+        while GPIO.input(self.dio):
+
+            time.sleep(0.001)
+
+            if GPIO.input(self.dio):
+                GPIO.setup(self.dio, GPIO.OUT)
+                GPIO.output(self.dio, GPIO.LOW)
+                GPIO.setup(self.dio, GPIO.IN)
+
+        GPIO.setup(self.dio, GPIO.OUT)
+
+
+    def refresh(self):
+
+        self.show_data(self.current_data)
+
+
+    def start(self):
+
+        GPIO.output(self.clk, GPIO.HIGH)
+        GPIO.output(self.dio, GPIO.HIGH)
+        GPIO.output(self.dio, GPIO.LOW)
+        GPIO.output(self.clk, GPIO.LOW)
+
+
+    def stop(self):
+
+        GPIO.output(self.clk, GPIO.LOW)
+        GPIO.output(self.dio, GPIO.LOW)
+        GPIO.output(self.clk, GPIO.HIGH)
+        GPIO.output(self.dio, GPIO.HIGH)
